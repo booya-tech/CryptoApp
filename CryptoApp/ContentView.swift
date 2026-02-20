692b0042d7f116c88a3d37214638983be4fde364
Boo Pannachai <booya.pannachai@gmail.com>
2026-02-20 13:46:59 +0700

    initial commit

---
+++ CryptoApp/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  CryptoApp
+//
+//  Created by Panachai Sulsaksakul on 2/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Accent Color")
+                    .foregroundStyle(Color.theme.accent)
+                
+                Text("Secondary Text Color")
+                    .foregroundStyle(Color.theme.secondaryText)
+                
+                Text("Red Color")
+                    .foregroundStyle(Color.theme.red)
+                
+                Text("Green Color")
+                    .foregroundStyle(Color.theme.green)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Panachai Sulsaksakul on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
