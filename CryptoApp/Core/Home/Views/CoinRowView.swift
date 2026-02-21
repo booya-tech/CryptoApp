78a0503527bc55a23a7ebb780601738c4b5d18dd
Boo Pannachai <booya.pannachai@gmail.com>
2026-02-21 14:39:26 +0700

    feat: add new extension for reusable mock data

---
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,22 @@
+//
+//  CoinRowView.swift
+//  CryptoApp
+//
+//  Created by Panachai Sulsaksakul on 2/21/26.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: Coin
+    
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    CoinRowView(
+        coin: DeveloperPreview.dev.coin
+    )
+}
